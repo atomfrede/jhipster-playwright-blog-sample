04d32eb83526fa7b3fd75ff16533f1c9fd4cf990
Frederik Hahne <atomfrede@users.noreply.github.com>
2021-03-22 16:31:40 +0100

    Update login-page.spec.ts

--- part-01/src/test/javascript/playwright/account/login-page.spec.ts
+++ part-01/src/test/javascript/playwright/account/login-page.spec.ts
@@ -2,13 +2,27 @@ import { LoginPage } from '../models/login';
 
 let loginPage: LoginPage;
 
-beforeEach(async () => {
+beforeAll(async () => {
   loginPage = new LoginPage(page);
   await loginPage.navigate();
 });
 
-test('should login', async () => {
+test('requires username', async () => {
+  await loginPage.login('', 'a-password', false);
+  await loginPage.loginErrorVisible();
+});
+
+test('requires password', async () => {
+  await loginPage.login('a-login', '', false);
+  await loginPage.loginErrorVisible();
+});
+
+test('errors when password is incorrect', async () => {
+  await loginPage.login('admin', 'bad-password', false);
+  await loginPage.loginErrorVisible();
+});
+
+test('go to login page when successfully logs in', async () => {
   await loginPage.login('admin', 'admin');
-  await page.goto('http://localhost:8080');
   await page.waitForSelector('[data-cy="adminMenu"]');
 });
